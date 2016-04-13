3a472ac651c4d0ece375950b32b3470a4b221892
Shen <shengum@gmail.com>
2016-04-13 00:25:38 -0400

    Updated memory game model

--- MemoryGameModel.java
+++ MemoryGameModel.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.util.*;
 import javax.swing.*;
 import java.awt.*;
@@ -9,7 +8,7 @@ import javax.imageio.ImageIO;
 
 public class MemoryGameModel extends GameModel{
 
-   public List<ImageIcon> images = new ArrayList<ImageIcon>();
+   public List<ImageIcon> images = new ArrayList<ImageIcon>(16);
    final int SIZE = 4;
    int numOpenDoors = 0;
    int numAttempts = 0;
@@ -24,7 +23,7 @@ public class MemoryGameModel extends GameModel{
     
       images.add(new ImageIcon("balloons.png"));
       images.add(new ImageIcon("birthday-girl.png"));
-      images.add(new ImageIcon("candles.png"));
+      images.add(new ImageIcon("candle.png"));
       images.add(new ImageIcon("candy.png"));
       images.add(new ImageIcon("hamburger.png"));
       images.add(new ImageIcon("musical-note.png"));
@@ -32,7 +31,7 @@ public class MemoryGameModel extends GameModel{
       images.add(new ImageIcon("present-1.png"));
       images.add(new ImageIcon("balloons.png"));
       images.add(new ImageIcon("birthday-girl.png"));
-      images.add(new ImageIcon("candles.png"));
+      images.add(new ImageIcon("candle.png"));
       images.add(new ImageIcon("candy.png"));
       images.add(new ImageIcon("hamburger.png"));
       images.add(new ImageIcon("musical-note.png"));
@@ -40,7 +39,7 @@ public class MemoryGameModel extends GameModel{
       images.add(new ImageIcon("present-1.png"));
    
    Collections.shuffle(images);
-   }
+   }//constructor
    
    public void takeTurn(int buttonChoice){
       //button choice lets this function know that a button (index of it) has been pressed
@@ -51,7 +50,7 @@ public class MemoryGameModel extends GameModel{
    }//take turn        
       
    boolean turnOverStatus(){
-      if (numOpenDoors == 2)
+      if (numOpenDoors == 2) //need new condition that accounts for opened doors that match
          return(true);
       else
          return(false); 
@@ -111,117 +110,4 @@ public class MemoryGameModel extends GameModel{
    //function: keep track of number of matches
    //talk to GUI for above
 
-=======
-import java.util.*;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-import java.io.*;
-import java.util.List;
-import javax.imageio.ImageIO;
-
-public class MemoryGameModel extends GameModel{
-
-   public List<ImageIcon> images = new ArrayList<ImageIcon>();
-   final int SIZE = 4;
-   int numOpenDoors = 0;
-   int numAttempts = 0;
-   int numMatches = 0;
-   int turnCount = 0;
-   boolean checkMatch = false;
-   boolean winner = false;
-   boolean turnOver = false;
-   boolean gameOver = false;
-   
-   MemoryGameModel(){
-    
-      images.add(new ImageIcon("balloons.png"));
-      images.add(new ImageIcon("birthday-girl.png"));
-      images.add(new ImageIcon("candles.png"));
-      images.add(new ImageIcon("candy.png"));
-      images.add(new ImageIcon("hamburger.png"));
-      images.add(new ImageIcon("musical-note.png"));
-      images.add(new ImageIcon("piano.png"));
-      images.add(new ImageIcon("present-1.png"));
-      images.add(new ImageIcon("balloons.png"));
-      images.add(new ImageIcon("birthday-girl.png"));
-      images.add(new ImageIcon("candles.png"));
-      images.add(new ImageIcon("candy.png"));
-      images.add(new ImageIcon("hamburger.png"));
-      images.add(new ImageIcon("musical-note.png"));
-      images.add(new ImageIcon("piano.png"));
-      images.add(new ImageIcon("present-1.png"));
-   
-   Collections.shuffle(images);
-   }
-   
-   public void takeTurn(int buttonChoice){
-      //button choice lets this function know that a button (index of it) has been pressed
-      //passed in from ActionPerformed
-      numOpenDoors++;
-      //if matching images, match+1; need to getPatt 
-      
-   }//take turn        
-      
-   boolean turnOverStatus(){
-      if (numOpenDoors == 2)
-         return(true);
-      else
-         return(false); 
-   }//1 turn over
-   
-   boolean gameOverStatus(){
-      if (numAttempts == 10 || numMatches == 8){
-         return(true);}
-      else{
-         return(false); }
-   }//game over
-   
-   public ImageIcon get(int i){
-      return(images.get(i));
-   }//get image
-   
-
-   int getRows(){
-      return(4);
-   }
-      
-   int getCols(){
-      return(SIZE);
-   }
-
-    
-   void display(){
-   
-   }
-         
-   String reportWinner(){
-      if (numMatches==8){
-         return("You win! ");
-      }
-      else{
-         return("Sorry! You lost!");
-      }
-   }
-   
-   int addAttempts(int turnCount){
-      return(numAttempts++);
-   }//number of attempts
-   
-   boolean checkMatch(int c1, int c2){
-      //if the paths of choice1 and choice2 are the same, they match, numMatches+1
-      //if paths don't match, they don't match
-      if (images.get(c2).equals(images.get(c2))){
-         return(true);
-      }
-      else{
-         return(false);
-      }
-   }//number of matches
-   
-   //function: get number of attempts to keep track
-   //function: keep track of number of matches
-   //talk to GUI for above
-
->>>>>>> origin/master
 }//class
