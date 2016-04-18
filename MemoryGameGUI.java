9483eacae3f31eccacf313c9b074c9a08a6b3ac8
nadwa310 <nadwa310@gmail.com>
2016-04-18 12:12:45 -0400

    Added files via upload
    not completed yet still some missing methods going to add them now

--- MemoryGameGUI.java
+++ MemoryGameGUI.java
@@ -7,192 +7,116 @@ import java.awt.Color;
 
 public class MemoryGameGUI extends JFrame implements ActionListener{
      
-   private final int WINDOW_WIDTH = 900;  
-   // Window width wide enough to not need to manually expand view to read all labels 
-   private final int WINDOW_HEIGHT = 600; // Window height
-   private int rows=0;
-   private int cols = 0;
-   private TextArea messageArea, attempts, attemptsLeft, matches;
-   private JButton [] doors  = new JButton[16];
-   private JButton reset = new JButton();
-   private MemoryGameModel dealGame; 
-   private String filler = " "; 
-   private JLabel result; 
-   private ImageIcon icon = new ImageIcon ("front.png");
-   private ImageIcon[] imagePair = new ImageIcon[2];
-   private int turnCount=0;
-   private boolean checkMatch;
-   private int numMatches;
+   private JTextArea messageTextArea;
+   private JTextField attemptCountTextField, attemptsRemainingTextField, matchCountTextField;
+   private JButton[] doors;
+   private static final MemoryGameModel MODEL = new MemoryGameModel();
+   private final static ImageIcon UNREVEALED_ICON= new ImageIcon ("front.png");
+   
+   private enum Selection {
+    FIRST, SECOND
+   };
+   
+   JButton firstMismatch = null, secondMismatch = null;
+   
+   private Selection currentSelection = Selection.FIRST;
 
    public MemoryGameGUI(){
-      setTitle("4x4 Memory Game");
-      setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-      
+      setTitle("Memory Game");
       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       
       // set layout and borders 
-      setLayout(new BorderLayout(15,15));
-      add(new JLabel(filler),BorderLayout.WEST);
-      add(new JLabel(filler),BorderLayout.EAST);
-      add(new JLabel(filler),BorderLayout.NORTH);
-      add(new JLabel(filler),BorderLayout.SOUTH);
-      add(new JLabel(filler),BorderLayout.CENTER);
-      
+      setLayout(new BorderLayout());
+
       Panel doorsPanel = new Panel ();
-      doorsPanel.setLayout(new GridLayout(4,4,5,5));
-      doorsPanel.setBackground(new Color(150,200,150));
-      
-      for(int i=0;i<16;i++){
+      doorsPanel.setLayout(new GridLayout(4,4));
+      for(int i=0;i<MODEL.getSize();i++){
          doors[i] = new JButton (icon);
          doorsPanel.add(doors[i]); 
          doors[i].addActionListener(this); 
       } 
       add(doorsPanel,BorderLayout.CENTER);
       
-      dealGame = new MemoryGameModel(); 
-      rows = dealGame.getRows(); 
-      cols = dealGame.getCols();
       
-      Panel bottomPanel = new Panel ();
-      bottomPanel.setLayout(new GridLayout(1,10,0,0));
-      bottomPanel.setBackground(new Color(150,230,150));
+      JPanel statisticsPanel = new JPanel ();
+      statisticsPanel.setLayout(new BoxLayout());
       
-            
-      bottomPanel.add(new JLabel("Attempts"));
-      attempts = new TextArea("0",1,2,TextArea.SCROLLBARS_NONE);
-      bottomPanel.add(attempts);
+      
+      statisticsPanel.add(new JLabel("Attempts"));
+      attempts = new TextArea("0");
+      statistcsPanel.add(attempts);
       attempts.setEditable(false);
       
-      bottomPanel.add(new JLabel(filler));
+      statisticsPanel.add(new JLabel(filler));
       
-      bottomPanel.add(new JLabel("Attempts Left"));
-      attemptsLeft = new TextArea("16",1,2,TextArea.SCROLLBARS_NONE);
-      bottomPanel.add(attemptsLeft);
+      statisticsPanel.add(new JLabel("Attempts Left"));
+      attemptsLeft = new TextArea(MODEL.getAttemptsRemaining());
+      statisticsPanel.add(attemptsLeft);
       attemptsLeft.setEditable(false);
       
-      bottomPanel.add(new JLabel(filler));
+      statisticsPanel.add(new JLabel(filler));
       
-      bottomPanel.add(new JLabel("Matches Made"));
-      matches = new TextArea("0",1,2,TextArea.SCROLLBARS_NONE); 
+      statisticsPanel.add(new JLabel("Matches Made"));
+      matches = new TextArea("0"); 
       bottomPanel.add(matches);
       matches.setEditable(false);
       
       bottomPanel.add(new JLabel(filler));
       
-      bottomPanel.add(new JButton("Reset"));
-      
       add(bottomPanel,BorderLayout.SOUTH);
 
       
-      result = new JLabel(filler); 
-      Panel topPanel = new Panel ();
-      topPanel.setLayout(new GridLayout(1,1,3,3));
-      topPanel.setBackground(new Color(150,130,150));
-      messageArea = new TextArea("Welcome!", 1,10,TextArea.SCROLLBARS_NONE);
+      messageArea = new TextArea("Welcome!");
       messageArea.setEditable(false);
       topPanel.add(messageArea);
       add(topPanel,BorderLayout.NORTH);
       
-      
+      pack();
       setVisible(true);
    } 
-   
-   public void BoarderLayout(int hgap, int vgap){}
-   
-   public void GridLayout(int row, int columns, int hgap, int vgap){}
-   
-   public void updateAttempts(int numAttempts){
-     //incrementing attempts is ActionPerformed's job, not this function's job - SG
-      String s = numAttempts+"";
-      attempts.setText(s);
-   }//update attempts
-   
-      
-   public void updateMatchesCount(int numMatches){
-      //this function only converts number of matches to display it as string - SG
-      String m = numMatches+"";
-      matches.setText(m);
-   }//update match count
-   
-   public void messageMatches(boolean checkMatch){
-      if(checkMatch){
-         result.setText("Match found!");
-      }
-      else{
-         result.setText("Not a match!");
-      }
-   }//msg matches
-   
-   //I rewrote updateFoundMatches below into two different functions:
-   //one updating message, one updating match count (shown above)
-   //the function below is trying to do too much work - SG
-   /*
-   public int updateFoundMatches(int numMatches, boolean checkMatch){
-      if (checkMatch == true){
-         result.setText("match found");
-         numMatches++;
-         String s = numMatches+"";
-         matches.setText(s);
-      }
-      else{
-         result.setText("not a match");
-      }
-      return numMatches;
-   } */
+ 
 
-
-   public void updateAttemptsLeft(int attempts){
-      int numAttemptsLeft = 16-attempts;
-      String s = numAttemptsLeft+"";
-      attemptsLeft.setText(s);
-   }
-   
-   public void resetDoors(){
-      for (int i=0;i<15;i++){
-         //doors[i].addActionListener(this);
-         doors[i].setIcon(icon);
-      }
-   }
-   
-   public int resetAttempts(int numAttempts){
-      numAttempts = 0;
-      String s = numAttempts+"";
-      attempts.setText(s);
-      return numAttempts;
-   }
-
-   public int resetAttemptsLeft(int numAttemptsLeft){
-      numAttemptsLeft = 0;
-      String s = numAttemptsLeft+"";
-      attemptsLeft.setText(s);
-      return numAttemptsLeft;
-   }
-   
-   public int resetMatches(int numMatches){
-      numMatches = 0;
-      String s = numMatches+"";
-      matches.setText(s);
-      return numMatches;
-   }
-   
    public void actionPerformed(ActionEvent click){
+    
       JButton buttonClicked = (JButton)click.getSource();
-      int attempts=dealGame.addAttempts(turnCount);
-      updateAttempts(attempts);
-      updateAttemptsLeft(attempts);
-      turnCount++;
-      int i=0;
-      while(buttonClicked != doors[i]){
-         i++; 
-      }//while finding source
-      doors[i].setIcon(dealGame.get(i));
-      dealGame.takeTurn(i);
-     
-      
-      if(turnCount%2==0){
-         updateMatchesCount(dealGame.getNumMatches());
-         //messageMatches(checkMatch);
+      if(MODEL.getAttemptsRemaining == 0)
+        return;
+        
+      switch(selection){
+        case FIRST:
+            if (firstMatch != null){
+                firstMatch.setIcon(UNREVEALED_ICON);
+                secondMatch.setIcon(UNREVEALED_ICON);
+                firstMatch = buttonClicked;
+                secondMatch = null;
+            }
+            
+            buttonClicked.setIcon(MODEL.getImageIcon(getIndex(buttonClicked)));
+            selection = Selection.SECOND;
+            break; 
+        case SECOND:
+            buttonClicked.setIcon(MODEL.getImageIcon(getIndex(buttonClicked)));
+            MODEL.incrementAttemptCount();
+            attemptCountTextField.setText(MODEL.getAttemptCount());
+            attemptsRemainingTextField.setText(MODEL.getAttemptsMax() - MODEL.getAttemptsCount());
+            selection = FIRST;
+            if (buttonClicked.getIcon().equal(firstMatch.getIcon())){
+                firstMatch = null;
+                MODEL.incrementMatch();
+                matchCountTextField.setText(MODEL.getMatchCount());
+            }
+            if (MODEL.getAttemptsRemaining == 0 || MODEL.getMatchCount() == MODEL.getMatchMax()){
+                messageTextArea.setText("Game over!");
+                if (MODEL.getMatchCount() == MODEL.getMatchMax()){
+                    JOptionPane.showMessageDialog(frame, "You win!");
+                } else {
+                    JOptionPane.showMessageDialog(frame, "You lose!");
+                }
+            }
+       
+            break;
       }
+      
 
-   }//action performed
+   }
 }
--- MemoryGameModel.java
+++ MemoryGameModel.java
@@ -8,18 +8,13 @@ import javax.imageio.ImageIO;
 
 public class MemoryGameModel extends GameModel{
 
-   public List<ImageIcon> images = new ArrayList<ImageIcon>(16);
-   final int SIZE = 4;
-   int numOpenDoors = 0;
-   public int numAttempts = 0;
-   public int numMatches = 0;
-   public int turnCount = 0;
-   public boolean checkMatch = false;
-   boolean winner = false;
-   boolean turnOver = false;
-   boolean gameOver = false;
-   private int choice1;
-   private int choice2;
+   
+   private final int matchTotal = 8;
+   private List<ImageIcon> images = new ArrayList<ImageIcon>(matchTotal *2);
+   private int numOpenDoors = 0;
+   private int numAttempts = 0;
+   private int matchCount = 0;
+   private int attemptsRemaining = 16;
    
    MemoryGameModel(){
       ImageIcon candle = new ImageIcon("candle.png");
@@ -48,79 +43,25 @@ public class MemoryGameModel extends GameModel{
       images.add(piano);
       images.add(present1); 
         
-   //Collections.shuffle(images);
-   }//constructor
-   
-   public void takeTurn(int i){
-      if(!turnOver){
-         choice1=i;
-      }
-      else{
-         choice2=i;
-         if(checkMatch(choice1, choice2)){
-            numMatches++;
-         }
-      }
-      turnOver = !turnOver;
-   }//take turn     
-   
-   public int getNumMatches(){
-      return(numMatches);
-   }//get num matches   
-   
-   boolean gameOverStatus(){
-      if (numAttempts == 10 || numMatches == 8){
-         return(true);}
-      else{
-         return(false);}
-   }//game over
-   
-   public ImageIcon get(int i){
-      return(images.get(i));
-   }//get image
-   
-
-   int getRows(){
-      return(4);
+        Collections.shuffle(images);
    }
-      
-   int getCols(){
-      return(SIZE);
+        
+   
+   public int getMatchCount(){
+      return matchCount;
+   } 
+   
+   public boolean isGameOver(){
+      return numAttempts == 10 || matchCount == 8;
+   }
+   
+   public ImageIcon getImageIcon(int i){
+      return images.get(i);
+   }
+   
+   public void incrementMatch(){
+       matchCount++;
    }
 
-    
-   void display(){
    
-   }
-         
-   String reportWinner(){
-      if (numMatches==8){
-         return("You win! ");
-      }
-      else{
-         return("Sorry! You lost!");
-      }
-   }
-   
-   int addAttempts(int turnCount){
-      if (turnCount%2==0){
-         return(numAttempts++);
-      }
-      else{
-         return(numAttempts);
-      }
-   }//number of attempts
-   
-   boolean checkMatch(int c1, int c2){
-      if (images.get(c1).equals(images.get(c2))){
-         return(true);
-      }
-      else{
-         return(false);
-      }
-   }//number of matches
-   
-   //function: get number of attempts to keep track
-   //function: keep track of number of matches
-
-}//class
+}
